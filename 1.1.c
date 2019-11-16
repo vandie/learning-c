8618ad4ba3f3efcd46e182d66bbd60591d7de18d
vandie <mikevdv@vandie.co.uk>
2019-11-16 19:38:26 +0000

    Initial Commit

---
+++ 1.1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world\n"); //C is strict about ; usage.
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Learning C
+My code for the tasks found in the ANSI C Programming Language Book (2nd Edition)
