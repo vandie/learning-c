217a8d4d385356571cfa0bc1252bf8e52015a77d
vandie <mikevdv@vandie.co.uk>
2019-11-16 21:37:33 +0000

    1.9

---
+++ section 1.5.3 - line counting/1.9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, lc;
+
+    while((c = getchar()) != EOF){
+        /*
+            if a char is not a space, output it.
+            If it is, ensure it's the first before outputting it
+        */
+        if(c != lc || c != ' ')
+            putchar(c);
+        lc = c;
+    }
+}
